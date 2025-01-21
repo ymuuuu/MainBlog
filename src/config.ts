913a7b30b12e8750c81585c9b0d4901ef7094ac1
Moamen Yasser <168992349+ymuuuu@users.noreply.github.com>
2025-01-21 22:57:31 +0200

    some new edits

--- src/config.ts
+++ src/config.ts
@@ -62,7 +62,7 @@ export const profileConfig: ProfileConfig = {
     },
     {
       name: 'Twitter',
-      icon: 'fa6-brands:twitter',       // Visit https://icones.js.org/ for icon codes
+      icon: 'fa6-brands:x-twitter',       // Visit https://icones.js.org/ for icon codes
                                         // You will need to install the corresponding icon set if it's not already included
                                         // `pnpm add @iconify-json/<icon-set-name>`
       url: 'https://x.com/ymuuu_',
