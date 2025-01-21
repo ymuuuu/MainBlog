e2ba5ba462b05e423a45828fdda56e5df9d938ca
Moamen Yasser <168992349+ymuuuu@users.noreply.github.com>
2025-01-21 14:39:11 +0200

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -11,11 +11,11 @@ export const siteConfig: SiteConfig = {
   subtitle: 'by Mu;men',
   lang: 'en',         // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko', 'es', 'th'
   themeColor: {
-    hue: 0,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
-    fixed: false,     // Hide the theme color picker for visitors
+    hue: 195,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
+    fixed: true,     // Hide the theme color picker for visitors
   },
   banner: {
-    enable: true,
+    enable: false,
     src: 'assets/images/demo-banner.png',   // Relative to the /src directory. Relative to the /public directory if it starts with '/'
     position: 'center',      // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
     credit: {
