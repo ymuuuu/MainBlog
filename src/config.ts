f498e77c71f33c3407a00d3bc2b0f1019fa1a96d
Moamen Yasser <168992349+ymuuuu@users.noreply.github.com>
2025-01-27 16:09:49 +0200

    Update config.ts
    remove banner

--- src/config.ts
+++ src/config.ts
@@ -15,7 +15,7 @@ export const siteConfig: SiteConfig = {
     fixed: true,     // Hide the theme color picker for visitors
   },
   banner: {
-    enable: true,
+    enable: false,
     src: 'assets/images/coverFHD.jpeg',   // Relative to the /src directory. Relative to the /public directory if it starts with '/'
     position: 'top',      // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
     credit: {
